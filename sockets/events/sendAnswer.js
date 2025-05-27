902e62277947f314a3d24c6d1ca58f01c6b4ea12
akashkhedar <akashkhedar262@gmail.com>
2025-05-27 23:11:04 +0530

    webrtc

--- sockets/events/sendAnswer.js
+++ sockets/events/sendAnswer.js
@@ -2,11 +2,7 @@ const { checkSocketId } = require("../../Utils/redis");
 
 const sendAnswer = async (callerId, calleeId, answer, io) => {
   const calleeSocket = await checkSocketId(calleeId);
-  io.to(calleeSocket).emit("getAnswer", {
-    callerId,
-    calleeId,
-    answer,
-  });
+  io.to(calleeSocket).emit("getAnswer", answer);
 };
 
 module.exports = sendAnswer;
